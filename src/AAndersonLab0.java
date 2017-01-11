b35656f2e03a951fcedddbfa8298bc7a331968a7
Aryk L Anderson <aryk.anderson@gmail.com>
2017-01-10 17:12:44 -0800

    First commit

---
+++ src/AAndersonLab0.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Aryk on 1/10/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AAndersonLab0 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
